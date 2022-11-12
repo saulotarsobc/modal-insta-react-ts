8bae410c6220ddf3c6260d3036f69885baef563e
saulotarsobc <saulotarsobc@gmail.com>
2022-11-12 10:48:44 -0300

    :fire:

--- src/components/InstaModal/index.tsx
+++ src/components/InstaModal/index.tsx
@@ -1,12 +1,45 @@
 import React from "react";
-import './index.scss';
+import "./index.scss";
+import { Close } from "./Icons";
 
 type InstaModalProps = {
-  title: String;
+  title: string;
+  srcImg: string;
+  description: string;
 };
 
-function InstaModal({ title }: InstaModalProps) {
-  return <div>{title}</div>;
+function InstaModal({ title, srcImg, description }: InstaModalProps) {
+  /* const openModal = () => {}; */
+
+  return (
+    <div className="insta-modal-wrap">
+      <div className="insta-modal">
+        <header className="modal-header">
+          <div className="title-wrap">
+            <img src={srcImg} alt="foto de perfil" />
+            <span>EntreFiosSTM</span>
+          </div>
+
+          <button
+            className="btn-close"
+            onClick={() => {
+              document
+                .querySelector(".insta-modal-wrap")
+                ?.classList.add("closed");
+            }}
+          >
+            <Close />
+          </button>
+        </header>
+
+        <main className="modal-main">
+          <img src={srcImg} alt={description} />
+        </main>
+
+        <footer className="modal-footer">footer</footer>
+      </div>
+    </div>
+  );
 }
 
 export default InstaModal;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import InstaModal from "./components/InstaModal";
 
+import images from "./images/img.jpg";
+
+import './index.scss';
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <InstaModal title="Título aki..." />
+    <InstaModal title="Título aki..." srcImg={images} description="description description..." />
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo distinctio ad nemo fuga aperiam voluptatem et voluptatum, veritatis quia alias libero a itaque aliquam assumenda labore nisi vero, ullam neque! Tenetur, voluptate in vitae aliquid omnis ipsam consectetur non officia doloribus? Facilis culpa saepe deleniti laudantium veniam optio explicabo incidunt odit tempora. Labore, eius. Quisquam, magni voluptatibus, sequi, qui eveniet ea quas totam dolorum omnis sapiente nihil tempore delectus adipisci repellendus. Earum reprehenderit quis voluptatem, ipsam autem iusto vitae suscipit voluptatibus temporibus modi! Repellendus non enim veritatis ullam praesentium odio rerum officiis atque. Enim iste esse iusto dicta iure officiis.
   </React.StrictMode>
 );
